70f4ef946adbefc123c5156bf00e1b61ba62fd59
KirillKalutsky <kefirchik.2000@mail.ru>
2022-02-21 23:46:43 +0500

    добавил gitignore

--- ArticleAnamlyzer/Program.cs
+++ ArticleAnamlyzer/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace TextAnalizator
 {
@@ -10,18 +11,41 @@ namespace TextAnalizator
     {
         static async Task Main(string[] args)
         {
+            var j = new JsonSerializerSettings();
+            j.MissingMemberHandling = MissingMemberHandling.Ignore;
+
+            var ser = JsonConvert.SerializeObject
+                (
+                    new List<Event> 
+                    {
+                        new Event() 
+                        {
+                            DateOfDownload = DateTime.Now, 
+                            IncidentCategory="cp",
+                            Title = "Title",
+                            Link = "link",
+                            Source = new Source()
+                        } 
+                    }
+                );
+
+
+            var des = JsonConvert.DeserializeObject<IEnumerable<Event>>(null, j);
+
+            Console.WriteLine(des);
+
             /*var a = new DistrictAnalyzer(new District[] {new District("район называется Правым") }, new Address[0]);
             var res = await a.AnalyzeDistrict("правый");
             Console.WriteLine(res.DistrictName);*/
+            /*
+                        var dict = new Dictionary<string, Dictionary<string, double>>() 
+                        {
+                            { "пожар", new Dictionary<string, double>(){ {"огонь",1 } } },
+                        };
 
-            var dict = new Dictionary<string, Dictionary<string, double>>() 
-            {
-                { "пожар", new Dictionary<string, double>(){ {"огонь",1 } } },
-            };
-
-            var b = new WeightAnalizator(dict, "Не ЧП");
-            var res = await b.AnalizeCategoryAsync("произошел пожар, много огня");
-            Console.WriteLine(res);
+                        var b = new WeightAnalizator(dict, "Не ЧП");
+                        var res = await b.AnalizeCategoryAsync("произошел пожар, много огня");
+                        Console.WriteLine(res);*/
         }
     }
 }

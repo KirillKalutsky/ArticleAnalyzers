75acbe44c2ec0dbd9a59022ff129bf1147077ec0
KirillKalutsky <kefirchik.2000@mail.ru>
2022-04-23 12:20:54 +0500

    изменил определение района по адресу

--- ArticleAnamlyzer/DistrictAnalyzer.cs
+++ ArticleAnamlyzer/DistrictAnalyzer.cs
@@ -7,55 +7,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using LemmaSharp.Classes;
 
 namespace TextAnalizator
 {
     public class DistrictAnalyzer
     {
-        private readonly Dictionary<string, District> districts;
+        private readonly Dictionary<string, HashSet<string>> districts;
+        private readonly Dictionary<string, HashSet<string>> lemmatizeDistricts;
         private readonly PythonExecutor pythonAnalyzer;
         private readonly string defaultDistrictName;
-        public DistrictAnalyzer(IEnumerable<District> districts, IEnumerable<Address> addresses, string defaultDistrictName = "none")
+        private readonly Lemmatizer lemmatizer;
+        public DistrictAnalyzer(Dictionary<string, HashSet<string>> districtNameByAddress,
+            PythonExecutor pythonExecutor, Lemmatizer lemmatizer, string defaultDistrictName = "none")
         {
             this.defaultDistrictName = defaultDistrictName;
-            this.districts = new Dictionary<string, District>();
-            foreach (var district in districts)
-                this.districts[district.DistrictName] = district;
-            foreach (var adr in addresses)
-                this.districts[adr.AddressName] = adr.District;
-            pythonAnalyzer = new PythonExecutor(@"D:\anaconda\python.exe", Path.GetFullPath(@"..\..\..\1.py"));
+            districts = districtNameByAddress;
+            pythonAnalyzer = pythonExecutor;
+            this.lemmatizer = lemmatizer;
+            lemmatizeDistricts = new Dictionary<string, HashSet<string>>();
+            foreach(var p in districts)
+            {
+                var words = new HashSet<string>();
+                lemmatizeDistricts[p.Key] = words;
+                foreach(var word in p.Value)
+                    words.Add(lemmatizer.Lemmatize(word));
+            }
         }
 
-        public async Task<District> AnalyzeDistrict(string text)
+        public async Task<string> AnalyzeDistrict(string text)
         {
             var res = await pythonAnalyzer.ExecuteScript(text);
-
+            var districtsStatistics = new Dictionary<string, int>();
             var output = JsonConvert.DeserializeObject<ScriptResponse>(res);
 
             if(output == null)
-                return districts[defaultDistrictName];
+                return defaultDistrictName;
+
+            var addresName = new HashSet<string>();
+            var lemAddresName = new HashSet<string>();
 
             if (output.Names != null)
             {
                 foreach (var name in output.Names)
-                {
-                    var nameL = name.ToLower();
-                    if (districts.ContainsKey(nameL))
-                        return districts[nameL];
-                }
+                    foreach (var n in name.Split(" "))
+                    {
+                        addresName.Add(n);
+                        lemAddresName.Add(lemmatizer.Lemmatize(n));
+                    }
             }
 
             if (output.Addresses != null)
-            {
-                foreach (var adr in output.Addresses)
-                {
-                    var adrName = adr.Value.ToLower();
-                    if (districts.ContainsKey(adrName))
-                        return districts[adrName];
-                }
-            }
+                foreach (var addr in output.Addresses)
+                    foreach (var n in addr.Value.Split(" "))
+                    {
+                        addresName.Add(n);
+                        lemAddresName.Add(lemmatizer.Lemmatize(n));
+                    }
 
-            return districts["none"];
+            await FindKeyWord(addresName, districts, districtsStatistics,1);
+
+            if(districtsStatistics.Any())
+                return districtsStatistics.OrderBy(d => d.Value).Last().Key;
+
+            await FindKeyWord(lemAddresName, lemmatizeDistricts, districtsStatistics, 1);
+
+            if (districtsStatistics.Any())
+                return districtsStatistics.OrderBy(d => d.Value).Last().Key;
+
+            return defaultDistrictName;
+        }
+
+        private Task FindKeyWord(IEnumerable<string> words, Dictionary<string, HashSet<string>> districtAddress, Dictionary<string, int> districtProbably, int weight)
+        {
+            return Task.Run(() =>
+            {
+                foreach (var adr in words)
+                {
+                    foreach (var a in adr.Trim().Split(" "))
+                    {
+                        var word = lemmatizer.Lemmatize(a.ToLower());
+                        foreach (var p in districtAddress)
+                        {
+                            if (p.Value.Contains(word))
+                                if (districtProbably.ContainsKey(p.Key))
+                                    districtProbably[p.Key] += weight;
+                                else
+                                    districtProbably[p.Key] = weight;
+                        }
+                    }
+                }
+            });
         }
     }
 }
--- ArticleAnamlyzer/Program.cs
+++ ArticleAnamlyzer/Program.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using LemmaSharp.Classes;
+using Python;
+using System.Linq;
 
 namespace TextAnalizator
 {
@@ -11,20 +14,60 @@ namespace TextAnalizator
     {
         static async Task Main(string[] args)
         {
-            
 
-            /*var a = new DistrictAnalyzer(new District[] {new District("район называется Правым") }, new Address[0]);
-            var res = await a.AnalyzeDistrict("правый");
-            Console.WriteLine(res.DistrictName);*/
-            /*
-                        var dict = new Dictionary<string, Dictionary<string, double>>() 
-                        {
-                            { "пожар", new Dictionary<string, double>(){ {"огонь",1 } } },
-                        };
+           
+            var python = new PythonExecutor(@"D:\anaconda\python.exe", @"D:\ВКР\Soft\ArticleAnamlyzer\ArticleAnamlyzer\1.py");
+            //var district = new Dictionary<string, Dictionary<string, double>>();
 
-                        var b = new WeightAnalizator(dict, "Не ЧП");
-                        var res = await b.AnalizeCategoryAsync("произошел пожар, много огня");
-                        Console.WriteLine(res);*/
+            var dataFilepath = @"D:\ВКР\Soft\ArticleAnamlyzer\ArticleAnamlyzer\full7z-mlteast-ru.lem";
+            Console.WriteLine(Path.GetFullPath(dataFilepath));
+            FileStream stream = File.OpenRead(dataFilepath);
+            var lemmatizer = new Lemmatizer(stream);
+            stream.Close();
+            var districtsNameByAddress = new Dictionary<string, HashSet<string>>();
+
+            var path = "D:\\ВКР\\streets_list_with_districts_update.csv";
+            using (var reader = new StreamReader(path))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    var street = values[1].ToLower().Trim().Split(" ");
+
+                    string districtName;
+                    var distr = values[2].Split(" ");
+                    if (distr.Length == 2)
+                        districtName = distr[1].ToLower().Trim();
+                    else
+                        districtName = values[2].ToLower().Trim();
+
+                    if (!districtsNameByAddress.ContainsKey(districtName))
+                        districtsNameByAddress[districtName] = new HashSet<string>() { districtName };
+
+                    foreach (var s in street)
+                    {
+                        //var word = lemmatizer.Lemmatize(s);
+                        districtsNameByAddress[districtName].Add(s);
+                    }
+
+                }
+            }
+
+            var text = "Днем 9 марта в Екатеринбурге на улице Щербакова сгорел пассажирский автобус «Богдан», который шел по маршруту №083. Пассажиры успели покинуть транспортное средство." +
+                "Как сообщили в ГУ МЧС России по Свердловской области, площадь пожара составила 20 квадратных метров. С огнем справились за 5 минут.В тушении возгорания были задействованы две единицы техники и 11 человек личного состава." +
+                "В администрации Екатеринбурга сообщили, что в салоне автобуса находилось 12 пассажиров.Именно они сообщили водителю о запахе гари.Водитель остановил автобус и высадил пассажиров." +
+                "Потушить возгорание автобуса первичными средствами не удалось.Пострадавших нет." +
+                "Ранее «МК - Урал» сообщал, что 6 декабря 2021 года в Екатеринбурге на перекрестке улиц Большакова и 8 Марта загорелся пассажирский автобус. Водитель и пассажиры успели покинуть транспортное средство." +
+                "В ГУ МЧС России по Свердловской области рассказали, что площадь пожара составила 4 квадратных метра, был поврежден моторный отсек автобуса «МАЗ».";
+
+            var districtAnalyzer = new DistrictAnalyzer(districtsNameByAddress, python, lemmatizer);
+
+            var result = await districtAnalyzer.AnalyzeDistrict(text);
+
+            Console.WriteLine(result);
         }
     }
 }
